3468105b9df2602734530dcb789334a70e7310c3
Kim Bergstrom <kimmen_b@hotmail.com>
2023-08-15 15:30:11 +0000

    update

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-appish-e594a7bc40b2.herokuapp.com', 'blog-appish']
+ALLOWED_HOSTS = ['blog-appish.herokuapp.com', 'blog-appish']
 
 
 # Application definition
