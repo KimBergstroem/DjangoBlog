fe716740eebae8d10c48bb5bbad669b7db320658
Kim Bergstrom <kimmen_b@hotmail.com>
2023-08-17 11:57:42 +0000

    Add: super user admin to the django panel

--- blog/settings.py
+++ blog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["blog-appish-e594a7bc40b2.herokuapp.com", "localhost", "8000-kimbergstroe-djangoblog-306ml80ttye.ws-eu103.gitpod.io"]
+ALLOWED_HOSTS = ["blog-appish-e594a7bc40b2.herokuapp.com", "localhost", '8000-kimbergstroe-djangoblog-306ml80ttye.ws-eu104.gitpod.io']
 
 
 # Application definition
