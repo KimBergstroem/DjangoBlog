a0ad8f05107ba942e5437c79d245d0448f49fcbf
Kim Bergstrom <kimmen_b@hotmail.com>
2023-09-06 20:38:02 +0000

    adsad

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
     path('post_profile/', profile_view, name='post_profile'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('newsletter/', views.NewsLetter, name='NewsLetter'),
-    path('validate/', views.validate_email, name='validate_email'),
 ]
 
 if settings.DEBUG:
